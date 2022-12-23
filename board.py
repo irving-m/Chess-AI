e633e40a4109c6072444bae3f6267293408cddf3
Irving <averageirving@gmail.com>
2022-12-23 02:36:19 -0500

    commit

--- board.py
+++ board.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from const import *
 from square import Square
+from piece import *
 
 class Board():
 
@@ -9,6 +10,8 @@ class Board():
         self.squares = np.zeros((N_ROWS, N_COLS))
 
         self._create()
+        self._add_piece("white")
+        self._add_piece("black")
 
     def _create(self):
         for row in range(N_COLS):
@@ -16,5 +19,23 @@ class Board():
                 self.squares[row][col] = Square(row, col)
 
     def _add_piece(self, color):
-        pass
+        if color == "white":
+            row_pawn, row_other = (6, 7)
+        else:
+            row_pawn, row_other = (1, 0)
 
+        for col in range(N_COLS):
+            self.squares[row_pawn] = Square(row_pawn, col, Pawn(color))
+        
+        self.squares[row_other][1] = Square(row_other, 1, Knight(color))
+        self.squares[row_other][6] = Square(row_other, 6, Knight(color))
+
+        self.squares[row_other][2] = Square(row_other, 2, Bishop(color))
+        self.squares[row_other][5] = Square(row_other, 5, Bishop(color))
+
+        self.squares[row_other][0] = Square(row_other, 0, Rook(color))
+        self.squares[row_other][7] = Square(row_other, 7, Rook(color))
+
+        self.squares[row_other][3] = Square(row_other, 3, Queen(color))
+
+        self.squares[row_other][4] = Square(row_other, 4, King(color))
--- piece.py
+++ piece.py
@@ -1,18 +1,23 @@
+import os
 
 class Piece():
-    def __init__(self, name, color, value, texture, texture_rect= None):
+    def __init__(self, name, color, value, texture= None, texture_rect= None):
         self.name = name
         self.color = color
-
+        self.moves = []
+        self.moved = False
         value_sign = 1 if color == "white" else -1
         self.value = value * value_sign
 
         self.set_texture()
         self.texture_rect = texture_rect
     
-    def set_texture():
-        pass
+    def set_texture(self, size= 80):
+        self.texture = os.path.join(
+            f"os.path.abspath('.'), 'assets/images/imgs-{size}px/{self.color}_{self.name}.png')")
 
+    def add_moves(self, move):
+        self.moves.append(move)
 
 class Pawn(Piece):
     def __init__(self, color):
